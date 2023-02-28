6b7c7d9a2974bc2de948c4c2e808cb2130815d2e
Farzan <farzanroxz123@gmail.com>
2023-02-28 10:13:30 +0530

    Completed till adding applicationtype to the product but there is an error

---
+++ WebApplication1AspDotNetCoreWebApp/Migrations/20230225120150_AddApplicationTypeToProduct.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1AspDotNetCoreWebApp.Migrations
+{
+    public partial class AddApplicationTypeToProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1AspDotNetCoreWebApp/Migrations/20230225170805_AddShortDescToProductTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1AspDotNetCoreWebApp.Migrations
+{
+    public partial class AddShortDescToProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "shortDesc",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "shortDesc",
+                table: "Product");
+        }
+    }
+}
